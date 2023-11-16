ed819ea349a5efd6f49c62aad9f68e4e1a9b475a
Jake <jacob.andreacchio@gmail.com>
2023-11-16 14:02:18 -0500

    structures the camera and updates movement

--- gamespace.py
+++ gamespace.py
@@ -5,11 +5,10 @@ pygame.init()
 
 screen = pygame.display.set_mode([500, 500])
 bg = pygame.image.load('World_map.png')
+pygame.display.set_caption('Legend of Link')
 
-pygame.display.set_caption('Incomplete')
-
-
-
+link_x = 232
+link_y = 232
 x = 50
 y = 50
 width = 20
@@ -18,9 +17,8 @@ vel = 1
 
 
 def redrawGameWindow():
-    global walkCount
-    screen.blit(bg, (0,0))
-    pygame.draw.rect(screen, (255, 255, 255), (x, y, width, height))
+    screen.blit(bg, (x, y))
+    pygame.draw.rect(screen, (255, 255, 255), (link_x, link_y, width, height))
     pygame.display.update()
     pygame.time.Clock().tick(165)
 
@@ -32,15 +30,54 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # camera movement
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]: x -= vel
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]: x += vel
-    if keys[pygame.K_UP] or keys[pygame.K_w]: y -= vel
-    if keys[pygame.K_DOWN] or keys[pygame.K_s]: y += vel
-    if x < 0: x = 0
-    if x > (500 - width): x = (500 - width)
-    if y < 0: y = 0
-    if y > (500 - height): y = (500 - height)
+    if link_x == 232:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            x += vel
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            x -= vel
+        if x < -780:
+            x = -780
+        if x > 0:
+            x = 0
+    if link_y == 232:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            y += vel
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            y -= vel
+        if y < -139:
+            y = -139
+        if y > 0:
+            y = 0
+
+    if x == -780 or x == 0:
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            link_x -= vel
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            link_x += vel
+        if link_x < 0:
+            link_x = 0
+        if link_x > 484:
+            link_x = 484
+    if y == -139 or y == 0:
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            link_y -= vel
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            link_y += vel
+        if link_y < 0:
+            link_y = 0
+        if link_y > 484:
+            link_y = 484
+
+    if x == 0 and link_x > 232:
+        link_x = 232
+    if y == 0 and link_y > 232:
+        link_y = 232
+    if x == -780 and link_x < 232:
+        link_x = 232
+    if y == -139 and link_y < 232:
+        link_y = 232
 
     redrawGameWindow()
