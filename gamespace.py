16342646fdc36efdcbf92516b6e4d3837d1a70b6
Jake <jacob.andreacchio@gmail.com>
2023-11-16 23:18:35 -0500

    adds new sprites

--- gamespace.py
+++ gamespace.py
@@ -5,40 +5,77 @@ pygame.init()
 
 # screen = pygame.display.set_mode(pygame.display.get_desktop_sizes()[0])
 screen = pygame.display.set_mode((500, 500))
-pygame.display.set_caption('Legend of Link')
+pygame.display.set_caption('Link\'s Happy Day')
 
 # sprites
 bg = pygame.image.load('World_map.png')
 walkDown = [pygame.image.load('Sprites/Link/D1.png'), pygame.image.load('Sprites/Link/D2.png')]
+# walkUp = [pygame.image.load('Sprites/Link/U1.png'), pygame.image.load('Sprites/Link/U2.png')]
+walkRight = [pygame.image.load('Sprites/Link/R1.png'), pygame.image.load('Sprites/Link/R2.png')]
+# walkLeft = [pygame.image.load('Sprites/Link/L1.png'), pygame.image.load('Sprites/Link/L2.png')]
 
-link_x = 232
-link_y = 232
-x = 50
-y = 50
-width = 20
-height = 30
-vel = 1
+bg_x = 50
+bg_y = 50
+width = 150
+height = 200
+link_x = (500-width)/2
+link_y = (500-height)/2
+vel = 10
 left = False
 right = False
 up = False
 down = False
 walkCount = 0
+last_image = pygame.image.load('Sprites/Link/D1.png')
 
 
 def redrawGameWindow():
     global walkCount
-    screen.blit(bg, (x, y))
-    # pygame.draw.rect(screen, (255, 255, 255), (link_x, link_y, width, height))
+    global last_image
+    screen.blit(bg, (bg_x, bg_y))
     # sprite animation
     if down:
         if walkCount < 15:
             screen.blit(walkDown[0], (link_x, link_y))
+            last_image = walkDown[0]
             walkCount += 1
         elif 15 <= walkCount < 30:
             screen.blit(walkDown[1], (link_x, link_y))
+            last_image = walkDown[1]
             walkCount += 1
         if walkCount == 30:
             walkCount = 0
+    elif right:
+        if walkCount < 15:
+            screen.blit(walkRight[0], (link_x, link_y))
+            last_image = walkRight[0]
+            walkCount += 1
+        elif 15 <= walkCount < 30:
+            screen.blit(walkRight[1], (link_x, link_y))
+            last_image = walkRight[1]
+            walkCount += 1
+        if walkCount == 30:
+            walkCount = 0
+    # elif up:
+    #     if walkCount < 15:
+    #         screen.blit(walkUp[0], (link_x, link_y))
+    #         walkCount += 1
+    #     elif 15 <= walkCount < 30:
+    #         screen.blit(walkUp[1], (link_x, link_y))
+    #         walkCount += 1
+    #     if walkCount == 30:
+    #         walkCount = 0
+    # elif left:
+    #     if walkCount < 15:
+    #         screen.blit(walkLeft[0], (link_x, link_y))
+    #         walkCount += 1
+    #     elif 15 <= walkCount < 30:
+    #         screen.blit(walkLeft[1], (link_x, link_y))
+    #         walkCount += 1
+    #     if walkCount == 30:
+    #         walkCount = 0
+    else:
+        screen.blit(last_image, (link_x, link_y))
 
     pygame.display.update()
     pygame.time.Clock().tick(60)
@@ -54,41 +91,41 @@ while True:
     # camera movement
     keys = pygame.key.get_pressed()
 
-    if link_x == 232:
+    if link_x == (500-width)/2:
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            x -= vel
+            bg_x -= vel
             right = True
             left = False
         elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            x += vel
+            bg_x += vel
             left = True
             right = False
         else:
             right, left = False, False
 
-        if x < -780:
-            x = -780
-        if x > 0:
-            x = 0
+        if bg_x < -780:
+            bg_x = -780
+        if bg_x > 0:
+            bg_x = 0
 
-    if link_y == 232:
+    if link_y == (500-height)/2:
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            y -= vel
+            bg_y -= vel
             down = True
             up = False
         elif keys[pygame.K_UP] or keys[pygame.K_w]:
-            y += vel
+            bg_y += vel
             up = True
             down = False
         else:
             up, down = False, False
 
-        if y < -139:
-            y = -139
-        if y > 0:
-            y = 0
+        if bg_y < -139:
+            bg_y = -139
+        if bg_y > 0:
+            bg_y = 0
 
-    if x == -780 or x == 0:
+    if bg_x == -780 or bg_x == 0:
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             link_x += vel
             right = True
@@ -102,10 +139,10 @@ while True:
 
         if link_x < 0:
             link_x = 0
-        if link_x > 484:
-            link_x = 484
+        if link_x > 351:
+            link_x = 350
 
-    if y == -139 or y == 0:
+    if bg_y == -139 or bg_y == 0:
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             link_y += vel
             down = True
@@ -118,16 +155,16 @@ while True:
             up, down = False, False
         if link_y < 0:
             link_y = 0
-        if link_y > 484:
-            link_y = 484
+        if link_y > 300:
+            link_y = 300
 
-    if x == 0 and link_x > 232:
-        link_x = 232
-    if y == 0 and link_y > 232:
-        link_y = 232
-    if x == -780 and link_x < 232:
-        link_x = 232
-    if y == -139 and link_y < 232:
-        link_y = 232
+    if bg_x == 0 and link_x > (500-width)/2:
+        link_x = (500-width)/2
+    if bg_y == 0 and link_y > (500-height)/2:
+        link_y = (500-height)/2
+    if bg_x == -780 and link_x < (500-width)/2:
+        link_x = (500-width)/2
+    if bg_y == -139 and link_y < (500-height)/2:
+        link_y = (500-height)/2
 
     redrawGameWindow()
